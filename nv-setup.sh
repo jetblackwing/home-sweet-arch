8ea4e114ebdcaf4ac5f7c8ec81e0acc9942f75f2
Amal JK <amaljk80@gmail.com>
2026-01-13 17:58:46 +0530

    Initial commit for nvidia setup. Idk if it works or not.

---
+++ nv-setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# An experimental setup for workaround with nvidia drivers on Arch Linux.
+# For basic and stable environment, we use suggested nvidia driver from AUR repo.
+# Usage: chmod +x nv-setup.sh && ./nv-setup.sh
+
+echo "Starting NVIDIA driver setup for Arch Linux..."
